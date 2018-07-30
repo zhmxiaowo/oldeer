cd33e3ab54f937d6e40b903f54bd3ab9a8144683
Osmin <260811192@qq.com>
2018-07-30 12:11:26 +0800

    提交项目

---
+++ Assets/oldeer/Olog/Olog.cs
@@ -0,0 +1,18 @@
+﻿/*===========================================================================
+ * Copyright (C) 2018-2021, 4DAGE Technology Co., Ltd. and/or its affiliates.
+ * All rights reserved.
+ * 
+ * author:  osmin
+ * time:    2018.7.27
+============================================================================*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//namespace oldeer;
+public class Olog {
+    public void Log()
+    {
+
+    }
+}
