c17b85ed55448bfea475cd24bf381c745585c071
Osmin <260811192@qq.com>
2018-08-02 12:02:30 +0800

    增加部分功能
    1.增加once初始化的功能
    2.增加文件读写帮助类

--- Assets/oldeer/flow/AppInit.cs
+++ Assets/oldeer/flow/AppInit.cs
@@ -14,8 +14,13 @@ public class AppInit : MonoBehaviour {
 
     //语言 0 中文 1 英文
     public int language = 0;
+
     private void Awake()
     {
+
+
+
+
         DontDestroyOnLoad(this.gameObject);
     }
     // Use this for initialization
@@ -23,5 +28,24 @@ public class AppInit : MonoBehaviour {
     {
 		
 	}
+
+    //保存调用方法
+    private static List<System.Type> addtionInitOnceList = new List<System.Type>();
+
+    /// <summary>
+    /// 只初始化一次的方法可以在这里附加调用,可以有效防止多次调用,程序运行到结束,多次调用,方法也只执行一次
+    /// </summary>
+    /// <param name="sender">调用的的主要类,通常为this</param>
+    /// <param name="action">调用的方法</param>
+    public static void AddInit(object sender,System.Action action)
+    {
+        System.Type type = sender.GetType();
+        if(!addtionInitOnceList.Contains(type))
+        {
+            addtionInitOnceList.Add(type);
+            //调用一次
+            action();
+        }
+    }
 
 }
--- Assets/oldeer/test/fm/testStart.cs
+++ Assets/oldeer/test/fm/testStart.cs
@@ -18,10 +18,10 @@ namespace test
             base.Awake();
 
             var v = TinyTeam.UI.TTUIPage.ShowPage<UIPanel>();               //根据类型去创建ui
-            TinyTeam.UI.TTUIPage.ShowPageByName<UIPanel>("name1");  //根据名字去创建相同ui
+            TinyTeam.UI.TTUIPage.ShowPageByName<UIPanel>("name1");          //根据名字去创建相同ui
             var v1 =TinyTeam.UI.TTUIPage.ShowPageByName<UIPanel>("name2");  //根据名字去创建相同的ui
             Debug.Log(v.gameObject.name);
-            Debug.Log(TinyTeam.UI.TTUIPage.GetPage("name2") == null);//根据名字去获取相同的ui
+            Debug.Log(TinyTeam.UI.TTUIPage.GetPage("name2") == null);       //根据名字去获取相同的ui
 
 
         }
