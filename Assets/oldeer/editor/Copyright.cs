5461380f0a50cb638a8c5fd286168c0df3f5fe92
osmin <zhmxiaowo@163.com>
2020-05-19 18:44:10 +0800

    update proj to 2018.4.14f ,remove cpoyright

--- Assets/oldeer/editor/Copyright.cs
+++ Assets/oldeer/editor/Copyright.cs
@@ -1,37 +1,37 @@
-﻿using UnityEngine;
-using System.Collections;
-using System.IO;
+﻿//using UnityEngine;
+//using System.Collections;
+//using System.IO;
 
-namespace oldeer
-{
-    public class Copyright : UnityEditor.AssetModificationProcessor
-    {
-        //作者
-        private static string author = "osmin";
+//namespace oldeer
+//{
+//    public class Copyright : UnityEditor.AssetModificationProcessor
+//    {
+//        //作者
+//        private static string author = "osmin";
 
-        //copyright
-        private static string str =
-@"/*===========================================================================
- * Copyright (C) 2018-2021, 4DAGE Technology Co., Ltd. and/or its affiliates.
- * All rights reserved.
- * 
- * author:  #author#
- * time:    #time#
-============================================================================*/
-";
-        // 创建资源调用
-        public static void OnWillCreateAsset(string path)
-        {
-            // 只修改C#脚本
-            path = path.Replace(".meta", "");
-            if (path.EndsWith(".cs"))
-            {
-                string allText = str;
-                allText += File.ReadAllText(path);
-                allText = allText.Replace("#author#", author);
-                allText = allText.Replace("#time#", System.DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss"));
-                File.WriteAllText(path, allText,System.Text.Encoding.UTF8);
-            }
-        }
-    }
-}
+//        //copyright
+//        private static string str =
+//@"/*===========================================================================
+// * Copyright (C) 2018-2021, 4DAGE Technology Co., Ltd. and/or its affiliates.
+// * All rights reserved.
+// * 
+// * author:  #author#
+// * time:    #time#
+//============================================================================*/
+//";
+//        // 创建资源调用
+//        public static void OnWillCreateAsset(string path)
+//        {
+//            // 只修改C#脚本
+//            path = path.Replace(".meta", "");
+//            if (path.EndsWith(".cs"))
+//            {
+//                string allText = str;
+//                allText += File.ReadAllText(path);
+//                allText = allText.Replace("#author#", author);
+//                allText = allText.Replace("#time#", System.DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss"));
+//                File.WriteAllText(path, allText,System.Text.Encoding.UTF8);
+//            }
+//        }
+//    }
+//}
